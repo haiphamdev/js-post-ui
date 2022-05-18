04737560fe573d7dd07a5ac710add31dc3017083
Hai Pham <haipham.ndev@gmail.com>
2022-05-19 01:01:34 +0700

    Fix CLS

--- js/utils/pagination.js
+++ js/utils/pagination.js
@@ -33,7 +33,7 @@ export function initPagination({ elementId, defaultParams, onChange }) {
     prevLink.addEventListener('click', (e) => {
       e.preventDefault();
       const page = Number.parseInt(ulPagination.dataset.page) || 1;
-      if (page > 2) onChange?.(page - 1);
+      if (page > 1) onChange?.(page - 1);
     });
   }
 
