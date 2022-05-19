bf6573f24b2f9dcb52bc61006a5b16040957d49d
Hai Pham <haipham.ndev@gmail.com>
2022-05-19 09:46:22 +0700

    Handle edit click

--- js/utils/post.js
+++ js/utils/post.js
@@ -25,8 +25,22 @@ export function createPostElement(post) {
   // go to post detail when click on div.post-item
   const divElement = liElement.firstElementChild;
   if (divElement) {
-    divElement.addEventListener('click', () => {
+    divElement.addEventListener('click', (event) => {
+      // S2: if event is trigger from menu --> ignore
+      const menu = liElement.querySelector('[data-id="menu"]');
+      if (menu && menu.contains(event.target)) return;
+
       window.location.assign(`/post-detail.html?id=${post.id}`);
+    });
+  }
+
+  // add click event for edit button
+  const editButton = liElement.querySelector('[data-id="edit"]');
+  if (editButton) {
+    editButton.addEventListener('click', (e) => {
+      // S1: prevent event bubbling to parent
+      // e.stopPropagation();
+      window.location.assign(`/add-edit-post.html?id=${post.id}`);
     });
   }
 
